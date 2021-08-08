d486b5ca1d9ad7d08a87aef25f00ebe5499317fe
Raghav Heda <raghav.heda@elastic.in>
2021-08-08 18:16:36 +0530

    Dothraki Speak

---
+++ index.js
@@ -0,0 +1,27 @@
+var inputText = document.querySelector("#txt-input");
+var translateText = document.querySelector("#txt-output");
+var btnTransalate = document.querySelector("#btn-translate");
+
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json";
+
+
+function getTranslation(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(){
+    alert("Some error occured in the server! Try after some time.")
+    console.log(error)
+}
+
+function clickHandler(){
+
+    function result(translation){
+        translateText.innerText = translation.contents.translated
+    }
+
+    var inputTxt = inputText.value;
+    fetch(getTranslation(inputTxt)).then(response=>response.json()).then(json=>result(json)).catch(errorHandler);    
+}
+
+btnTransalate.addEventListener("click",clickHandler);
